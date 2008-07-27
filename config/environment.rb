c41d4d3481c43d756616b897b722b19f436eaf49
Ryan Lowe <ryanlowe@gmail.com>
2008-07-27 15:48:44 -0400

    move mailers and observers out of models directory

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,10 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  config.load_paths += %W[
+    #{RAILS_ROOT}/app/mailers
+    #{RAILS_ROOT}/app/observers
+  ]
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
