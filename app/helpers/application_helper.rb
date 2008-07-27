ea774912745f1fcdd9995383b6eadbae982b51d9
Ryan Lowe <ryanlowe@gmail.com>
2008-07-27 16:12:49 -0400

    add basic layout and stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def format_head_title(title)
+    return SITE_NAME if title.to_s.length < 1
+    h(title)+" - "+SITE_NAME
+  end
+  
 end
