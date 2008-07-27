2c605b5046d9f7edcfc18608b7039fd367ede3dd
Ryan Lowe <ryanlowe@gmail.com>
2008-07-27 15:00:20 -0400

    add exception_notification plugin

---
+++ config/initializers/exception_notifier.rb
@@ -0,0 +1,4 @@
+# ExceptionNotifier plugin configuration
+ExceptionNotifier.exception_recipients = %w(rails@ryanlowe.ca)
+ExceptionNotifier.sender_address = %("Exception" <rails@ryanlowe.ca>)
+ExceptionNotifier.email_prefix = ""
