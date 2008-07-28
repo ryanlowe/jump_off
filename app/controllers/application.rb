177f3a6ef20be53907c435ba0d5d2332059d667e
Ryan Lowe <ryanlowe@gmail.com>
2008-07-28 16:27:43 -0400

    change User#login to User#username

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   
-  before_filter :login_from_cookie
-  
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'acd2581d7e3d4c751b9c86b6db8d0699'
-  
+  protect_from_forgery :secret => 'acd258158b2d4c7512d446b6db771e99'  
   filter_parameter_logging :password
+  
 end
