287daab42e0428e9b8584d8ccf180ee1284ac837
Ryan Lowe <ryanlowe@gmail.com>
2008-07-28 16:46:54 -0400

    remove more cookie and http auth cruft from auth library

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -1,5 +1,7 @@
 module AuthenticatedSystem
+  
   protected
+  
     # Returns true or false if the user is logged in.
     # Preloads @current_user with the user model if they're logged in.
     def logged_in?
@@ -17,22 +19,6 @@ module AuthenticatedSystem
       @current_user = new_user
     end
     
-    # Check if the user is authorized.
-    #
-    # Override this method in your controllers if you want to restrict access
-    # to only a few actions or if you want to check if the user
-    # has the correct rights.
-    #
-    # Example:
-    #
-    #  # only allow nonbobs
-    #  def authorize?
-    #    current_user.username != "bob"
-    #  end
-    def authorized?
-      true
-    end
-
     # Filter method to enforce a login requirement.
     #
     # To require logins for all actions, use this in your controllers:
@@ -48,9 +34,7 @@ module AuthenticatedSystem
     #   skip_before_filter :login_required
     #
     def login_required
-      username, passwd = get_auth_data
-      self.current_user ||= User.authenticate(username, passwd) || :false if username && passwd
-      logged_in? && authorized? ? true : access_denied
+      logged_in? ? true : access_denied
     end
     
     # Redirect as appropriate when an access request fails.
@@ -63,7 +47,7 @@ module AuthenticatedSystem
     # simply close itself.
     def access_denied
       store_location
-      redirect_to :controller => '/account', :action => 'login'
+      redirect_to login_url
       false
     end  
     
@@ -87,25 +71,4 @@ module AuthenticatedSystem
       base.send :helper_method, :current_user, :logged_in?
     end
 
-    # When called with before_filter :login_from_cookie will check for an :auth_token
-    # cookie and log the user back in if apropriate
-    def login_from_cookie
-      return unless cookies[:auth_token] && !logged_in?
-      user = User.find_by_remember_token(cookies[:auth_token])
-      if user && user.remember_token?
-        user.remember_me
-        self.current_user = user
-        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
-        flash[:notice] = "Logged in successfully"
-      end
-    end
-
-  private
-    @@http_auth_headers = %w(X-HTTP_AUTHORIZATION HTTP_AUTHORIZATION Authorization)
-    # gets BASIC auth info
-    def get_auth_data
-      auth_key  = @@http_auth_headers.detect { |h| request.env.has_key?(h) }
-      auth_data = request.env[auth_key].to_s.split unless auth_key.blank?
-      return auth_data && auth_data[0] == 'Basic' ? Base64.decode64(auth_data[1]).split(':')[0..1] : [nil, nil] 
-    end
 end
