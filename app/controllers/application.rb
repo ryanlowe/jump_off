be3bf6132ba382ea3914888abfee0c64597cdc02
Ryan Lowe <ryanlowe@gmail.com>
2008-07-29 14:13:10 -0400

    move admin views to views/admin

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   
+  # protect against forged cross-site scripting attacks
   protect_from_forgery :secret => 'acd258158b2d4c7512d446b6db771e99'  
+  
+  # don't show passwords in the clear in logs
   filter_parameter_logging :password
   
+  # return 404 to unauthenticated requests for public pages until launch
   before_filter :launch_required
+  
+  # for admin views
+  prepend_view_path(RAILS_ROOT+"/app/views/admin")
   
 end
