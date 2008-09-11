fbb80abdabeb1b3b5a3c71cfc2dd0e546cf16dc0
Ryan Lowe <ryanlowe@gmail.com>
2008-09-11 08:41:30 -0400

    record last touched datetime, show in admin

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -18,10 +18,21 @@ class ApplicationController < ActionController::Base
   # use Eastern Time by default
   before_filter :set_time_zone
   
+  # record the last time the user hit the site
+  before_filter :set_touch
+  
   protected
   
     def set_time_zone
       Time.zone = "Eastern Time (US & Canada)"
+    end
+    
+    def set_touch
+      if logged_in? and !current_user.nil?
+        current_user.last_touched_at = Time.now.utc
+        current_user.save_or_raise
+      end
+      true
     end
   
 end
