52b9d26af414a1d434387991561dfeb1f1c45b2e
Ryan Lowe <ryanlowe@gmail.com>
2008-09-11 08:12:40 -0400

    helpers should escape link text

--- app/helpers/admin/admin_users_helper.rb
+++ app/helpers/admin/admin_users_helper.rb
@@ -1,6 +1,6 @@
 module AdminUsersHelper
   def admin_link_user(user)
     return "" if user.nil?
-    link_to user.username, :controller => 'admin_users', :action => 'show', :id => user
+    link_to h(user.username), :controller => 'admin_users', :action => 'show', :id => user
   end
 end
